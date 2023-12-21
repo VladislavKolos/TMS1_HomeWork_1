842c4d9c2a33bcc4de38b08c9dd9e7c015c0ae04
Vladislav_14 <vladislav.kolos.y@gmail.com>
2023-12-22 00:10:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+double b = 3.5;
+double c = 2.7;
+double a = 4*(b + c - 1) / 2;
+        System.out.println("a = " + a);
+
+    }
+}
